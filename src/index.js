f6f2448086a4889018eb48a1bdd8fb12d5262953
Simon Sinding <sinding2000@gmail.com>
2020-08-09 21:00:31 +0200

    feat: adds store to handle state

--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import AppContainer from './components/AppContainer';
-import App from './components/App';
-import './sass/_main.scss';
+import React from "react";
+import ReactDOM from "react-dom";
+import AppContainer from "./components/AppContainer";
+import App from "./components/App";
+import "./sass/_main.scss";
+import { Provider } from "react-redux";
+import store from "./store";
 
 ReactDOM.render(
+  <Provider store={store}>
     <AppContainer>
-        <App/>
-    </AppContainer>,
-    document.getElementById('root')
+      <App />
+    </AppContainer>
+  </Provider>,
+  document.getElementById("root")
 );
