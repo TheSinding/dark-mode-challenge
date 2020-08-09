05ed932c0b91679304ab1e4fc16755846f1accd5
Simon Sinding <sinding2000@gmail.com>
2020-08-09 21:01:52 +0200

    feat: adds dark mode toggling to the theme switcher

--- src/partials/ThemeSwitcher.js
+++ src/partials/ThemeSwitcher.js
@@ -1,15 +1,34 @@
-import React from 'react';
-import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
-import {faMoon} from '@fortawesome/free-solid-svg-icons';
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faMoon, faSun } from "@fortawesome/free-solid-svg-icons";
+import { connect } from "react-redux";
+import { toggleDarkMode } from "../store/application/actions";
 
-export default class ThemeSwitcher extends React.Component {
+const mapStateToProps = ({ settings }) => ({
+  darkMode: settings.darkMode,
+});
 
-    render() {
-        return (
-            <button className="app__dark-mode-btn icon level-right">
-                <FontAwesomeIcon icon={faMoon} color={'#4D5B6B'}/>
-            </button>
-        );
-    }
+const mapDispatchToProps = (dispatch) => ({
+  toggleDarkMode: () => dispatch(toggleDarkMode()),
+});
 
+class ThemeSwitcher extends React.Component {
+  icon = () => {
+    return this.props.darkMode ? faSun : faMoon;
+  };
+  color = () => {
+    return this.props.darkMode ? "#FFA500" : "#4D5B6B";
+  };
+  render() {
+    return (
+      <button
+        className="app__dark-mode-btn icon level-right"
+        onClick={this.props.toggleDarkMode}
+      >
+        <FontAwesomeIcon icon={this.icon()} color={this.color()} />
+      </button>
+    );
+  }
 }
+
+export default connect(mapStateToProps, mapDispatchToProps)(ThemeSwitcher);
