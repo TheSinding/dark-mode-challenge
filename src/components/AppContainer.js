7669946ff907f14b914e4676e85fea55a07b1031
Simon Sinding <sinding2000@gmail.com>
2020-08-09 21:01:27 +0200

    feat: adds store to app container.

--- src/components/AppContainer.js
+++ src/components/AppContainer.js
@@ -1,15 +1,21 @@
-import React from 'react';
+import React from "react";
+import { connect } from "react-redux";
 
-export default class AppContainer extends React.Component {
-
-    render() {
-        const {children} = this.props;
-
-        return (
-            <div className="app-container">
-                {children}
-            </div>
-        );
+const mapStateToProps = ({ settings }) => ({
+  darkMode: settings.darkMode,
+});
+class AppContainer extends React.Component {
+  classNames = () => {
+    const classes = ["app-container"];
+    if (this.props.darkMode) {
+      classes.push("dark-mode");
     }
-
+    return classes.join(" ");
+  };
+  render() {
+    const { children } = this.props;
+    return <div className={this.classNames()}>{children}</div>;
+  }
 }
+
+export default connect(mapStateToProps)(AppContainer);
